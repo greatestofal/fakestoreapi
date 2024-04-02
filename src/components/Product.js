6c496b0d674f17e5c361dd47e3a70643a882e0a5
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-02 11:50:57 +0100

    on product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useContext } from "react";
+// import link
+import { Link } from "react-router-dom";
+//import icons
+import { BsPlus, BsEyeFill } from "react-icons/bs";
+const Product = ({ product }) => {
+  // destructure produts
+  const { id, title, price, image, category } = product;
 
-const Product = () => {
-  return <div>Product</div>;
+  return (
+    <div>
+      <div className="border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition">
+        <div className="w-full h-full flex justify-center items-center">
+          {/* Image */}
+          <div className="w-[200px] mx-auto flex justify-center items-center">
+            <img
+              className="max-h-[160px] group-hover:scale-110 transtion duration-300"
+              src={image}
+              alt={title}
+            />
+          </div>
+        </div>
+      </div>
+      <div>2</div>
+    </div>
+  );
 };
 
 export default Product;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,9 @@ import React, { useContext } from "react";
 
 // import product
 import { ProductContext } from "../contexts/ProductContext";
+
+import Product from "../components/Product";
+
 const Home = () => {
   //get product from product context
   const { products } = useContext(ProductContext);
@@ -9,20 +12,19 @@ const Home = () => {
   // get only men's & women's clothing category
   const filteredProducts = products.filter((item) => {
     return (
-      item.category === "Men's Clothing" || item.category === "Women's Clothing"
+      item.category === "men's clothing" || item.category === "women's clothing"
     );
   });
 
   return (
-    <section>
+    <section className="py-16">
       <div className="container mx-auto">
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px] max-w-sm mx-auto md:max-w-none md:mx-0">
           {filteredProducts.map((product) => {
-            return <div key={product.id}>{product.title}</div>;
+            return <Product product={product} key={product.id} />;
           })}
         </div>
       </div>
-      <div> Homepage </div>
     </section>
   );
 };
