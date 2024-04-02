b0cdd75674d9db67e4de56fa09f41acd9caf5893
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-02 16:05:35 +0100

    Second

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,11 @@ import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 //import icons
 import { BsPlus, BsEyeFill } from "react-icons/bs";
+// import cart context
+import { CartContext } from "../contexts/CartContext";
+
 const Product = ({ product }) => {
+  const { addToCart } = useContext(CartContext);
   // destructure produts
   const { id, title, price, image, category } = product;
 
@@ -20,8 +24,30 @@ const Product = ({ product }) => {
             />
           </div>
         </div>
+        {/* Button */}
+        <div className="absolute top-6 right-5 group-hover:right-5 p-2 flex flex-col items-center justify-self-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
+          <button onClick={() => addToCart(product, id)}>
+            <div className="flex justify-center items-center text-white w-12 h-12 bg-red-500">
+              <BsPlus className="text-3xl" />
+            </div>
+          </button>
+          <Link
+            to={`/product/${id}`}
+            className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl
+          "
+          >
+            <BsEyeFill />
+          </Link>
+        </div>
       </div>
-      <div>2</div>
+      {/* Categories & Title & Price */}
+      <div>
+        <div className="text-sm capitalize text-gray-500"> {category} </div>
+        <Link to={`/products/${id}`}>
+          <h2 className="font-semibold mb-1"> {title} </h2>
+        </Link>
+        <div className="font-semibold">$ {price}</div>
+      </div>
     </div>
   );
 };
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from "react";
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+//create context
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  //cart state
+  const [cart, setCart] = useState([]);
+  //add to cart
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+    //check if the item is already in the cart
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    // if cart item is already in the cart
+    if (cartItem) {
+      const newCart = [...cart].map((item) => {
+        if (item.id === id) {
+          return { ...item, amount: cartItem.amount + 1 };
+        }
+      });
+    }
+  };
+  return (
+    <CartContext.Provider value={{ addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
 
-export default CartContext;
+export default CartProvider;
