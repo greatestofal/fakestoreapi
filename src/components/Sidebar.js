1528f4eabd67a9478f9f2be77ae0c3c8fc8a4dec
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-04-08 15:11:17 +0100

    Final commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -18,7 +18,7 @@ const Sidebar = () => {
         isOpen ? "right-0" : "-right-full"
       } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`}
     >
-      <div className="flex items-center justify-between py-6 border-b">
+      <div className="flex items-center justify-between py-2 border-b">
         <div className="uppercase text-sm font-semibold">
           {" "}
           Shopping Bag ({itemAmount})
@@ -31,7 +31,7 @@ const Sidebar = () => {
           <IoMdArrowRoundForward className="text-2xl" />
         </div>
       </div>
-      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto overflow-x-hidden border-b">
+      <div className="flex flex-col gap-y-2 h-[520px] lg:h-[510px] overflow-y-auto overflow-x-hidden border-b">
         {cart.map((item) => {
           return <CartItem item={item} key={item.id} />;
         })}
